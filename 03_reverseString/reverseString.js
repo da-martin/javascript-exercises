969d17e088a4c9f4e7635bb93fb875d8577a66ee
da-martin <martindavidandreas@gmail.com>
2022-03-09 16:02:52 +0100

    Exercise 03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,14 @@
 const reverseString = function(str = "") {
-  let reversedString = "";
+  let reversedStr = [];
   let count = 0;
-  for (i = str.length; i > 0; i--){
-    reverseString[count] = str[i];
+  for (i = str.length-1; i >= 0; i--){
+  //  console.log(" i:" + i);
+  //  console.log(" c:" + count);
+  //  console.log(" str:" + str[i]);
+    reversedStr.push(str[i]);
     count++;
   }
-
-
+  return reversedStr.join("");
 };
 
 // Do not edit below this line
