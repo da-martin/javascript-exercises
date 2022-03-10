50676e5e7ee2040beee9115dbce7705d1cd32a6f
da-martin <martindavidandreas@gmail.com>
2022-03-10 18:02:10 +0100

    Exercise 04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,21 @@
-const removeFromArray = function(arr) {
-  let result = [];
-    
-  //For each parameter after the first, 
-  //which should be the array
+const removeFromArray = function() {
+  let result = arguments[0];
+  //For each parameter after the first 
+  //(which should be the array)
   for (let a = 1; a < arguments.length; a++) {
+    //console.log("Argument: " + arguments[a]);
     //Compare with every element in the array
-    for (i = 0; i < arr.length; i++) {
-      if (arr[i] === arguments[a]) arr.splice(i);
+    for (i = 0; i < result.length; i++) {
+      //console.log("  Array element: " + result[i]);
+      if (result[i] === arguments[a]) {
+      //console.log("  This is a match:" + result[i] + " = " + arguments[a]);
+        result.splice(i,1);
+        i--;
+      }
     }  
   }
+  //console.log("Final array: " + result);
+  return result;
 };
 
 // Do not edit below this line
