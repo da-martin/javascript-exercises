91ce28d464296dc54d948aecc77fa62ee554a44d
da-martin <martindavidandreas@gmail.com>
2022-03-10 18:22:12 +0100

    Exercise 05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+  let result = 0;
+  let c = 0;
+  ///console.log ("a: " + a);
+  //console.log ("b: " + b);
+  if ((typeof(a)!="number") || a < 0) return "ERROR";
+  if ((typeof(b)!="number") || b < 0) return "ERROR";
+  if (a > b){
+    c = a;
+    a = b;
+    b = c;
+  }
+  for (let i = a; i <= b; i++)
+    result += i;
 
+  return result;
 };
 
 // Do not edit below this line
